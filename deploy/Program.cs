aa32bd831a1e39e66653df676a69221dd9906f86
Travis Harris <travistylerharris@outlook.com>
2017-12-06 13:43:59 -0800

    Adding deployment script for node server

--- deploy/Program.cs
+++ deploy/Program.cs
@@ -10,41 +10,80 @@ namespace Deploy
     {
         static void Main(string[] args)
         {
+            // const string deploymentSource = @"D:\home\site\repository";
+            // const string deploymentTarget = @"D:\home\site\wwwroot";
+            // var deploymentTempTarget = GetTempFolder();
+            // const string toolsDirectory = @"D:\home\tools";
+            // const string ng = @"node_modules\.bin\ng";
+            // var yarn = Path.Combine(toolsDirectory, "yarn");
+            // var msBuild = @"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe".DoubleQuote();
+            // var assets = Path.Combine(deploymentSource, @"AzureFunctions.AngularClient\src\assets");
+
+            // TryCreateDirectory(deploymentTempTarget);
+            // TryCreateDirectory($@"{deploymentTempTarget}\ng-min");
+            // TryCreateDirectory($@"{deploymentTempTarget}\ng-full");
+
+            // DeploySdk
+            //     .StandardDeployment
+            //     .Call("npm", $"config set prefix {toolsDirectory}")
+            //     .Call("npm", "install -g yarn")
+            //     .Call("nuget", $"restore {deploymentSource}\\AzureFunctions.sln")
+            //     .Call(msBuild, $@"{deploymentSource}\AzureFunctions\AzureFunctions.csproj /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir=""%DEPLOYMENT_TEMP%"";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false  /p:DeleteExistingFiles=False /p:SolutionDir=""{deploymentSource}\.\\"" %SCM_BUILD_ARGS%")
+            //     .KuduSync()
+            //     .ChangeDirectory($@"{deploymentSource}\AzureFunctions.AngularClient")
+            //     .Call(yarn, "install", tries: 2)
+            //     .Call("npm", "rebuild node-sass")
+            //     .CleanAngularArtifacts()
+            //     .Call(ng, $"build --progress false --prod --environment=prod --output-path=\"{deploymentTempTarget}\\ng-min\"", tries: 2)
+            //     .Call(ng, $"build --progress false --output-path=\"{deploymentTempTarget}\\ng-full\"", tries: 2)
+            //     .CopyDirectory($@"{deploymentTempTarget}\ng-min", $@"{deploymentTarget}\ng-min")
+            //     .CopyDirectory($@"{deploymentTempTarget}\ng-full", $@"{deploymentTarget}\ng-full")
+            //     .UpdateCshtml()
+            //     .SetupTemplatesWebJob()
+            //     .UpdateBuildTxt()
+            //     .CopyFile($@"{assets}\googlecdaac16e0f037ee3.html", $@"{deploymentTarget}\googlecdaac16e0f037ee3.html")
+            //     .CopyDirectory($@"{assets}\schemas", $@"{deploymentTarget}\schemas")
+            //     .OnFail(EmailHelpers.EmailFailedRun)
+            //     .OnSuccess(EmailHelpers.EmailSuccessfulRun)
+            //     .Run();
+
             const string deploymentSource = @"D:\home\site\repository";
             const string deploymentTarget = @"D:\home\site\wwwroot";
             var deploymentTempTarget = GetTempFolder();
             const string toolsDirectory = @"D:\home\tools";
             const string ng = @"node_modules\.bin\ng";
             var yarn = Path.Combine(toolsDirectory, "yarn");
-            var msBuild = @"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe".DoubleQuote();
+            var gulp = @"node_modules\.bin\gulp";
+            var tsc =  @"node_modules\.bin\tsc";
             var assets = Path.Combine(deploymentSource, @"AzureFunctions.AngularClient\src\assets");
 
-            TryCreateDirectory(deploymentTempTarget);
+            TryCreateDirectory($@"{deploymentTempTarget}");
             TryCreateDirectory($@"{deploymentTempTarget}\ng-min");
             TryCreateDirectory($@"{deploymentTempTarget}\ng-full");
+            TryCreateDirectory($@"{deploymentTempTarget}\node_modules");
 
             DeploySdk
                 .StandardDeployment
                 .Call("npm", $"config set prefix {toolsDirectory}")
                 .Call("npm", "install -g yarn")
-                .Call("nuget", $"restore {deploymentSource}\\AzureFunctions.sln")
-                .Call(msBuild, $@"{deploymentSource}\AzureFunctions\AzureFunctions.csproj /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir=""%DEPLOYMENT_TEMP%"";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false  /p:DeleteExistingFiles=False /p:SolutionDir=""{deploymentSource}\.\\"" %SCM_BUILD_ARGS%")
-                .KuduSync()
+                .ChangeDirectory($@"{deploymentSource}\server")
+                .Call(yarn, "install", tries: 2)
+                .Call(tsc, "", tries: 2)
+                .CopyDirectory($@"{deploymentSource}\server\build",  $@"{deploymentTempTarget}")
+                .CopyFile($@"{deploymentSource}\server\iisnode.yml",  $@"{deploymentTempTarget}\iisnode.yml")
+                .CopyFile($@"{deploymentSource}\server\web.config",  $@"{deploymentTempTarget}\web.config")
+                .CopyFile($@"{deploymentSource}\server\package.json",  $@"{deploymentTempTarget}\package.json")
                 .ChangeDirectory($@"{deploymentSource}\AzureFunctions.AngularClient")
                 .Call(yarn, "install", tries: 2)
                 .Call("npm", "rebuild node-sass")
                 .CleanAngularArtifacts()
                 .Call(ng, $"build --progress false --prod --environment=prod --output-path=\"{deploymentTempTarget}\\ng-min\"", tries: 2)
                 .Call(ng, $"build --progress false --output-path=\"{deploymentTempTarget}\\ng-full\"", tries: 2)
-                .CopyDirectory($@"{deploymentTempTarget}\ng-min", $@"{deploymentTarget}\ng-min")
-                .CopyDirectory($@"{deploymentTempTarget}\ng-full", $@"{deploymentTarget}\ng-full")
-                .UpdateCshtml()
-                .SetupTemplatesWebJob()
-                .UpdateBuildTxt()
+                .CopyDirectory($@"{deploymentTempTarget}", $@"{deploymentTarget}")
                 .CopyFile($@"{assets}\googlecdaac16e0f037ee3.html", $@"{deploymentTarget}\googlecdaac16e0f037ee3.html")
                 .CopyDirectory($@"{assets}\schemas", $@"{deploymentTarget}\schemas")
-                .OnFail(EmailHelpers.EmailFailedRun)
-                .OnSuccess(EmailHelpers.EmailSuccessfulRun)
+                .ChangeDirectory(deploymentTarget)
+                .Call(yarn, "install --production", tries: 2)
                 .Run();
         }
 
